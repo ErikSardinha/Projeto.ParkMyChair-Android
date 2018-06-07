a5c9945da153562567505c9daacea08de2ae5a46
Erik Santos Sardinha <erik.sardinha@hotmail.com>
2018-06-07 19:19:05 -0300

    Registro + Login

--- app/src/main/java/com/example/familialima/parkchair/LoginActivity.java
+++ app/src/main/java/com/example/familialima/parkchair/LoginActivity.java
@@ -8,13 +8,22 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
+        Button btRegister = (Button) findViewById(R.id.btRegister);
         Button btLogar = (Button) findViewById(R.id.btLogar);
+        final Intent register = new Intent(this,RegisterActivity.class);
+        //final Intent map = new Intent(this, MapActivity);
+        btRegister.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                startActivity(register);
+            }
+        });
         btLogar.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
@@ -25,6 +34,9 @@ public class MainActivity extends AppCompatActivity {
                 //Teste de Logar
                 if (login.equals("teste")&&senha.equals("teste")){
                     alert("Login Realizado com Sucesso");
+
+                    //startActivity(map);
+                   //finish();
                 }else{
                     alert("Usuário ou Senha incorretos");
                 }
